b054984370322b9b8846321ca92d6ecbdf5ca206
shams <aniknishan@gmail.com>
2020-10-12 21:29:03 +0600

    logged in and printed title

--- src/main/java/org/webscrapper/App.java
+++ src/main/java/org/webscrapper/App.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import org.jsoup.Connection.Method;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 /**
  * Hello world!
@@ -12,10 +13,21 @@ import org.jsoup.Jsoup;
 public class App
 {
     public static void main( String[] args ) throws IOException {
+        Response loginPageResponse =
+            Jsoup.connect("https://firstbd.ltd/work/index.php")
+                .followRedirects(true)
+                .execute();
+
         Response response =
             Jsoup.connect("https://firstbd.ltd/work/index.php")
                 .data("username", "nsi30")
-                .data("password", "nsi#20").method(Method.POST).execute();
-        System.out.println(response.body());
+                .data("password", "nsi#30")
+                .cookies(loginPageResponse.cookies())
+                .followRedirects(true)
+                .method(Method.POST)
+                .execute();
+
+        Document doc = response.parse();
+        System.out.println(doc.getElementsByClass("navbar-brand").get(0).html());
     }
 }
