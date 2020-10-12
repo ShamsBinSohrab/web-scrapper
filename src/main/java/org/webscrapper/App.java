17273ed1ca9aa37b7933f4338e511f944d540f3e
shams <aniknishan@gmail.com>
2020-10-12 22:11:47 +0600

    added login service

--- src/main/java/org/webscrapper/App.java
+++ src/main/java/org/webscrapper/App.java
@@ -1,10 +1,8 @@
 package org.webscrapper;
 
 import java.io.IOException;
-import org.jsoup.Connection.Method;
-import org.jsoup.Connection.Response;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.webscrapper.service.LoginService;
 
 /**
  * Hello world!
@@ -13,21 +11,7 @@ import org.jsoup.nodes.Document;
 public class App
 {
     public static void main( String[] args ) throws IOException {
-        Response loginPageResponse =
-            Jsoup.connect("https://firstbd.ltd/work/index.php")
-                .followRedirects(true)
-                .execute();
-
-        Response response =
-            Jsoup.connect("https://firstbd.ltd/work/index.php")
-                .data("username", "nsi30")
-                .data("password", "nsi#30")
-                .cookies(loginPageResponse.cookies())
-                .followRedirects(true)
-                .method(Method.POST)
-                .execute();
-
-        Document doc = response.parse();
+        Document doc = LoginService.login().parse();
         System.out.println(doc.getElementsByClass("navbar-brand").get(0).html());
     }
 }
---
+++ src/main/java/org/webscrapper/service/LoginService.java
@@ -0,0 +1,54 @@
+package org.webscrapper.service;
+
+import java.io.IOException;
+import java.util.Scanner;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+
+public class LoginService {
+
+  public static Response login() throws IOException {
+    final LoginCredentials credentials = new LoginCredentials();
+    Response loginPageResponse =
+        Jsoup.connect(credentials.getLoginUrl())
+            .followRedirects(true)
+            .execute();
+    return Jsoup.connect("https://firstbd.ltd/work/index.php")
+            .data("username", credentials.getUsername())
+            .data("password", credentials.getPassword())
+            .cookies(loginPageResponse.cookies())
+            .followRedirects(true)
+            .method(Method.POST)
+            .execute();
+  }
+
+  private static class LoginCredentials {
+    private final String loginUrl;
+    private final String username;
+    private final String password;
+
+    public LoginCredentials() {
+      Scanner scanner = new Scanner(System.in);
+      loginUrl = "https://firstbd.ltd/work/index.php";
+
+      System.out.println("Username: ");
+      this.username = scanner.next();
+
+      System.out.println("Password: ");
+      this.password = scanner.next();
+    }
+
+    public String getLoginUrl() {
+      return loginUrl;
+    }
+
+    public String getUsername() {
+      return username;
+    }
+
+    public String getPassword() {
+      return password;
+    }
+  }
+}
