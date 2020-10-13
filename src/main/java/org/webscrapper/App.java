6d9a387d8fbca629bba06e8453a1d676a38d8b68
shams <aniknishan@gmail.com>
2020-10-13 19:26:14 +0600

    read excel sheet and printed in stdout

--- src/main/java/org/webscrapper/App.java
+++ src/main/java/org/webscrapper/App.java
@@ -1,10 +1,16 @@
 package org.webscrapper;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import org.jsoup.nodes.Document;
+import java.util.Iterator;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.webscrapper.service.FileService;
-import org.webscrapper.service.LoginService;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  * Hello world!
@@ -12,11 +18,33 @@ import org.webscrapper.service.LoginService;
  */
 public class App
 {
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) throws IOException, InvalidFormatException {
         File file = FileService.getInstance().choose();
-        System.out.print(file.getName());
+        FileInputStream inputStream = new FileInputStream(file);
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(0);
 
-        Document doc = LoginService.getInstance().login().parse();
-        System.out.println(doc.getElementsByClass("navbar-brand").get(0).html());
+        Iterator<Row> rowIterator = sheet.iterator();
+
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                if (cell.getCellType().equals(CellType.NUMERIC)) {
+                    System.out.print(cell.getNumericCellValue() + " ");
+                } else if (cell.getCellType().equals(CellType.STRING)) {
+                    System.out.print(cell.getStringCellValue() + " ");
+                } else {
+                    System.out.print("Other -> " + cell.getCellType().name() + " ");
+                }
+            }
+            System.out.println("\n");
+        }
+
+//        Document doc = LoginService.getInstance().login().parse();
+//        System.out.println(doc.getElementsByClass("navbar-brand").get(0).html());
+
+
     }
 }
